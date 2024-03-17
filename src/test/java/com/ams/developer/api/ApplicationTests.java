35b482d9027801d66ecbc20f75dedf01381f0c19
Axel200121 <axelmartinez212001@gmail.com>
2024-03-16 20:07:34 -0600

    Creación del proyecto

---
+++ src/test/java/com/ams/developer/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.developer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
